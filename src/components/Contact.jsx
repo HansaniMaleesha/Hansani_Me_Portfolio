bda2c709b45c390b247d66a123586f945173ac58
Hansani Maleesha <124916072+HansaniMaleesha@users.noreply.github.com>
2024-12-01 15:03:09 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-      'service_qrwo94w', 'template_pctct7d', form.current, 'ImS2E91cN-RXlL86R'
+      'service_uf0kgsk', 'template_pctct7d', form.current, 'ImS2E91cN-RXlL86R'
       )
       .then(
         (result) => {
